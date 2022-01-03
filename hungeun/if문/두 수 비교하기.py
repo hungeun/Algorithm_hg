db2836c1efc9cd4e571f69623f5ded672dbb2a69
hungeun <91244381+hungeun@users.noreply.github.com>
2022-01-03 20:10:14 +0900

    Add files via upload

---
+++ hungeun/if문/두 수 비교하기.py
@@ -0,0 +1,10 @@
+intput_data = input().split(' ')
+a = int(intput_data[0])
+b = int(intput_data[1])
+
+if a > b:
+    print('>')
+elif  a < b:
+    print('<')
+else:
+    print('==')       
---
+++ hungeun/if문/시험성적.py
@@ -0,0 +1,12 @@
+score =  int(input())
+   
+if score >= 90:
+    print('a')
+elif score >= 80:
+    print('b')    
+elif score >= 70:
+    print('c')
+elif score >= 60:
+    print('d')
+else:
+    print('f')
